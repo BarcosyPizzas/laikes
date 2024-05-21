070f05af93ceda74f3252e3e5be4724eea6ccb44
BarcosyPizzas <67290371+BarcosyPizzas@users.noreply.github.com>
2024-05-21 08:57:26 -0700

    si

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('bresenhamCanvas');
+const ctx = canvas.getContext('2d');
+
+function lineaBresenham(x0, y0, x1, y1){
+    let dx = x1 - x0;
+    let dy = y1 - y0;
+    let D = 2*dy - dx;
+    y = y0; 
+    
+
+}
